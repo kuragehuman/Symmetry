a620c95b8f7af8626b608e234147a9ad7582a2d7
kuragehuman <kuragehuman@gmail.com>
2019-06-10 03:15:30 +0900

    change program

--- symmetry.cpp
+++ symmetry.cpp
@@ -23,41 +23,16 @@ int main(int argc, char const *argv[]){
 	
 	VideoCapture cap(0);
 	Mat image;
-	Mat def_omen = imread("Omen.png");
 	
-	CascadeClassifier cascade;
-	cascade.load("haarcascade_frontalface_alt.xml");
-	Mat grayimage;
-
 	while (1) {
 		cap >> image;
 
-		//処理時間短縮のためのサイズ変更
-		Mat smallImage;
-		resize(image, smallImage, Size(), IMGSMALL, IMGSMALL);
-		cvtColor(smallImage, grayimage, CV_RGB2GRAY);
-
-		vector<Rect> faces;
-		cascade.detectMultiScale(grayimage, faces, 1.1, 3, 0, Size(20, 20));
-
-		for(int i=0; i<faces.size(); i++){
-			faces[i].x -= 5/IMGSMALL;
-			faces[i].y -= 5/IMGSMALL;
-			faces[i].width += 10/IMGSMALL;
-			faces[i].height += 10/IMGSMALL;
-			faces[i].x /= IMGSMALL;
-			faces[i].y /= IMGSMALL;
-			faces[i].width /= IMGSMALL;
-			faces[i].height /= IMGSMALL;
-			Mat omen;
-			resize(def_omen, omen, Size(faces[i].width, faces[i].height));
-	
-			if((faces[i].x >= 0) && (faces[i].x + omen.cols <= image.cols) && (faces[i].y >= 0) && (faces[i].y + omen.rows <= image.rows)){
-				Mat roi = image(Rect(faces[i].x, faces[i].y, omen.cols, omen.rows));
-				omen.copyTo(roi);
+		for(int i = image.cols/2; i < image.cols; i++){
+			for(int j = 0; j < image.rows; j++){
+				image.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(j, image.cols - i);//image(j, image.cols/2 - i)[0];
 			}
 		}
-		imshow("Virtual YouTuber", image);
+		imshow("symmetry", image);
 		waitKey(1);
 
 	}
