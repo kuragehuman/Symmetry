d60c8d24c3a535868f8fdcd7d650151495da4bfb
kuragehuman <kuragehuman@gmail.com>
2019-06-10 17:42:27 +0900

    fix program

--- symmetry.cpp
+++ symmetry.cpp
@@ -1,23 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-#define IMGSMALL 0.25
-
 using namespace std;
 using namespace cv;
-
-Mat detectFaceInImage(Mat &image,string &cascade_file){
-	CascadeClassifier cascade;
-	cascade.load(cascade_file);
-
-	vector<Rect> faces;
-	cascade.detectMultiScale(image, faces, 1.1,3,0,Size(20,20));
-
-	for (int i = 0; i < faces.size(); i++){
-		rectangle(image, Point(faces[i].x,faces[i].y),Point(faces[i].x + faces[i].width,faces[i].y + faces[i].height),Scalar(0,200,0),3,CV_AA);
-	}
-	return image;
-}
 
 int main(int argc, char const *argv[]){
 	
